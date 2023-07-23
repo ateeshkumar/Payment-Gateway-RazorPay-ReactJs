0c67d72ad00f838c431c0166721b9ea4c0686fb3
ateeshkumar <ateesh2002@gmail.com>
2023-07-24 01:43:55 +0530

    first-comiit

---
+++ src/App.js
@@ -0,0 +1,208 @@
+import './App.css';
+import useRazorpay from "react-razorpay";
+function App() {
+  const [Razorpay] = useRazorpay();
+  const displayRazorPay = (amount)=>{
+    
+    var options={
+      key:"",
+      key_secret:"",
+      amount:amount*100,
+      currency:'INR',
+      name:"Ateesh Foundation",
+      description:"For Testing Purpose",
+      prefill: {
+        name: "Kumar Ateesh ",
+        email: "youremail@example.com",
+        contact: "8004640159",
+      },
+      notes: {
+        address: "Razorpay Corporate Office",
+      },
+      theme: {
+        color: "#3399cc",
+      },
+
+    }
+    var pay = new window.Razorpay(options);
+    pay.on("payment.failed", function (response) {
+      alert(response.error.code);
+      alert(response.error.description);
+      alert(response.error.source);
+      alert(response.error.step);
+      alert(response.error.reason);
+      alert(response.error.metadata.order_id);
+      alert(response.error.metadata.payment_id);
+    });
+    pay.open();
+  }
+  return (
+    <div className="App">
+    <h1>Welcome Shopping Mobile</h1>
+    <div className="payment"></div>
+        {
+          products.map((item,idx) => (
+            <div className="img" key={idx}>
+              <img src={item.thumbnail} alt="Logo" />
+              <div className="details">
+               <h3>{item.title}</h3>
+               <h4>Price: {item.price} Rs</h4>
+               <h6>Rating: {item.rating} ⭐</h6>
+               <p>Company: {item.brand}</p>
+               <p>Details: {item.description}</p>
+              </div>
+              <button onClick={()=>displayRazorPay(item.price)} className='btn'>Buy Now</button>
+            </div>
+          ))
+        }
+    </div>
+  );
+}
+
+export default App;
+const products= [
+  {
+      "id": 1,
+      "title": "iPhone 9",
+      "description": "An apple mobile which is nothing like apple",
+      "price": 2,
+      "discountPercentage": 12.96,
+      "rating": 4.69,
+      "stock": 94,
+      "brand": "Apple",
+      "category": "smartphones",
+      "thumbnail": "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
+      "images": [
+          "https://i.dummyjson.com/data/products/1/1.jpg",
+          "https://i.dummyjson.com/data/products/1/2.jpg",
+          "https://i.dummyjson.com/data/products/1/3.jpg",
+          "https://i.dummyjson.com/data/products/1/4.jpg",
+          "https://i.dummyjson.com/data/products/1/thumbnail.jpg"
+      ]
+  },
+  {
+      "id": 2,
+      "title": "iPhone X",
+      "description": "SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...",
+      "price": 899,
+      "discountPercentage": 17.94,
+      "rating": 4.44,
+      "stock": 34,
+      "brand": "Apple",
+      "category": "smartphones",
+      "thumbnail": "https://i.dummyjson.com/data/products/2/thumbnail.jpg",
+      "images": [
+          "https://i.dummyjson.com/data/products/2/1.jpg",
+          "https://i.dummyjson.com/data/products/2/2.jpg",
+          "https://i.dummyjson.com/data/products/2/3.jpg",
+          "https://i.dummyjson.com/data/products/2/thumbnail.jpg"
+      ]
+  },
+  {
+      "id": 3,
+      "title": "Samsung Universe 9",
+      "description": "Samsung's new variant which goes beyond Galaxy to the Universe",
+      "price": 1249,
+      "discountPercentage": 15.46,
+      "rating": 4.09,
+      "stock": 36,
+      "brand": "Samsung",
+      "category": "smartphones",
+      "thumbnail": "https://i.dummyjson.com/data/products/3/thumbnail.jpg",
+      "images": [
+          "https://i.dummyjson.com/data/products/3/1.jpg"
+      ]
+  },
+  {
+      "id": 4,
+      "title": "OPPOF19",
+      "description": "OPPO F19 is officially announced on April 2021.",
+      "price": 280,
+      "discountPercentage": 17.91,
+      "rating": 4.3,
+      "stock": 123,
+      "brand": "OPPO",
+      "category": "smartphones",
+      "thumbnail": "https://i.dummyjson.com/data/products/4/thumbnail.jpg",
+      "images": [
+          "https://i.dummyjson.com/data/products/4/1.jpg",
+          "https://i.dummyjson.com/data/products/4/2.jpg",
+          "https://i.dummyjson.com/data/products/4/3.jpg",
+          "https://i.dummyjson.com/data/products/4/4.jpg",
+          "https://i.dummyjson.com/data/products/4/thumbnail.jpg"
+      ]
+  },
+  {
+      "id": 5,
+      "title": "Huawei P30",
+      "description": "Huawei’s re-badged P30 Pro New Edition was officially unveiled yesterday in Germany and now the device has made its way to the UK.",
+      "price": 499,
+      "discountPercentage": 10.58,
+      "rating": 4.09,
+      "stock": 32,
+      "brand": "Huawei",
+      "category": "smartphones",
+      "thumbnail": "https://i.dummyjson.com/data/products/5/thumbnail.jpg",
+      "images": [
+          "https://i.dummyjson.com/data/products/5/1.jpg",
+          "https://i.dummyjson.com/data/products/5/2.jpg",
+          "https://i.dummyjson.com/data/products/5/3.jpg"
+      ]
+  },
+  {
+      "id": 6,
+      "title": "MacBook Pro",
+      "description": "MacBook Pro 2021 with mini-LED display may launch between September, November",
+      "price": 1749,
+      "discountPercentage": 11.02,
+      "rating": 4.57,
+      "stock": 83,
+      "brand": "Apple",
+      "category": "laptops",
+      "thumbnail": "https://i.dummyjson.com/data/products/6/thumbnail.png",
+      "images": [
+          "https://i.dummyjson.com/data/products/6/1.png",
+          "https://i.dummyjson.com/data/products/6/2.jpg",
+          "https://i.dummyjson.com/data/products/6/3.png",
+          "https://i.dummyjson.com/data/products/6/4.jpg"
+      ]
+  },
+  {
+      "id": 7,
+      "title": "Samsung Galaxy Book",
+      "description": "Samsung Galaxy Book S (2020) Laptop With Intel Lakefield Chip, 8GB of RAM Launched",
+      "price": 1499,
+      "discountPercentage": 4.15,
+      "rating": 4.25,
+      "stock": 50,
+      "brand": "Samsung",
+      "category": "laptops",
+      "thumbnail": "https://i.dummyjson.com/data/products/7/thumbnail.jpg",
+      "images": [
+          "https://i.dummyjson.com/data/products/7/1.jpg",
+          "https://i.dummyjson.com/data/products/7/2.jpg",
+          "https://i.dummyjson.com/data/products/7/3.jpg",
+          "https://i.dummyjson.com/data/products/7/thumbnail.jpg"
+      ]
+  },
+  {
+      "id": 8,
+      "title": "Microsoft Surface Laptop 4",
+      "description": "Style and speed. Stand out on HD video calls backed by Studio Mics. Capture ideas on the vibrant touchscreen.",
+      "price": 1499,
+      "discountPercentage": 10.23,
+      "rating": 4.43,
+      "stock": 68,
+      "brand": "Microsoft Surface",
+      "category": "laptops",
+      "thumbnail": "https://i.dummyjson.com/data/products/8/thumbnail.jpg",
+      "images": [
+          "https://i.dummyjson.com/data/products/8/1.jpg",
+          "https://i.dummyjson.com/data/products/8/2.jpg",
+          "https://i.dummyjson.com/data/products/8/3.jpg",
+          "https://i.dummyjson.com/data/products/8/4.jpg",
+          "https://i.dummyjson.com/data/products/8/thumbnail.jpg"
+      ]
+  }
+];
+
